1ae31a0b74ce1b1042a6885510a0229820cae09c
ikram98ai <ikram98ai@gmail.com>
2025-06-10 23:13:47 +0500

    adding terrafrom backend

--- llm-finetuning/compliance_verification/terraform/backend.tf
+++ llm-finetuning/compliance_verification/terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-20250610"  # Replace with your S3 bucket
     key            = "apparel-fastapi-lambda/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"         # Replace with your DynamoDB table
   }
