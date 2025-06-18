7eaba67afcbb39bd7842ecb874bbe1cd8ff820c0
ikram98ai <ikram98ai@gmail.com>
2025-06-18 22:20:10 +0500

    increasing lambda timeout because o3 is slow

--- llm-finetuning/compliance_verification/terraform/main.tf
+++ llm-finetuning/compliance_verification/terraform/main.tf
@@ -125,7 +125,7 @@ resource "aws_lambda_function" "fastapi_lambda" {
   role         = aws_iam_role.lambda_role.arn
   package_type = "Image"
   image_uri    = "${aws_ecr_repository.apparel_fastapi_repo.repository_url}:${var.image_tag}"
-  timeout      = 30
+  timeout      = 90
   memory_size  = 512
   architectures = ["x86_64"]
 
